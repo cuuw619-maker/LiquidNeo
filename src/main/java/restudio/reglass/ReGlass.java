99ef3afcb9752d9668404ea1b0c05292786e749e
cuuw619-maker <cuuw619@gmail.com>
2026-09-10 19:19:35 +0300

    Guard ReGlass client initialization by distribution

--- src/main/java/restudio/reglass/ReGlass.java
+++ src/main/java/restudio/reglass/ReGlass.java
@@ -2,11 +2,11 @@ package restudio.reglass;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import restudio.reglass.client.ReGlassClient;
 
 @Mod(ReGlass.MOD_ID)
 public class ReGlass {
@@ -16,8 +16,8 @@ public class ReGlass {
     public ReGlass(IEventBus modEventBus) {
         LOGGER.info("Initializing ReGlass {} on {}", MOD_ID, FMLEnvironment.dist);
 
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            ReGlassClient.register(modEventBus);
-        }
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () ->
+                restudio.reglass.client.ReGlassClient.register(modEventBus)
+        );
     }
 }
