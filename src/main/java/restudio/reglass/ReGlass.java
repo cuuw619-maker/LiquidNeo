f47adc8df2ce02bbe5de0bb2bf1ed5358e02a846
cuuw619-maker <cuuw619@gmail.com>
2026-09-10 19:49:00 +0300

    Fix NeoForge client registration API

--- src/main/java/restudio/reglass/ReGlass.java
+++ src/main/java/restudio/reglass/ReGlass.java
@@ -2,7 +2,6 @@ package restudio.reglass;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
@@ -15,9 +14,8 @@ public class ReGlass {
 
     public ReGlass(IEventBus modEventBus) {
         LOGGER.info("Initializing ReGlass {} on {}", MOD_ID, FMLEnvironment.dist);
-
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () ->
-                restudio.reglass.client.ReGlassClient.register(modEventBus)
-        );
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            restudio.reglass.client.ReGlassClient.register(modEventBus);
+        }
     }
 }
